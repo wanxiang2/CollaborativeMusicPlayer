618353768850f9ad97f7fedfbd881b8f31f5814f
wanxiang2 <wanxiang@buffalo.edu>
2024-08-13 16:43:13 -0400

    Created API Endpoints for when the user clicks on the create room button.

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Room from "./Room";
 
 export default class HomePage extends Component {
   constructor(props) {
@@ -15,6 +16,7 @@ export default class HomePage extends Component {
           <Route path="/" element={<p>This is the home page</p>} />
           <Route path="/join" element={<RoomJoinPage />} />
           <Route path="/create" element={<CreateRoomPage />} />
+          <Route path="/room/:roomCode" element={<Room />} />
         </Routes>
       </Router>
     );
